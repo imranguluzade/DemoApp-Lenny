8d753f68d58fc0dc501555266accf092ff28921c
ImranGuluzada <imranguluzadeh@gmail.com>
2023-08-13 15:45:29 +0400

    appjsx edited

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,8 @@ const App = () => {
   return (
     <div>
       <Header />
-<<<<<<< HEAD
       <Footer/>
       <PopularProdCart/>
-=======
-      <Footer />
->>>>>>> d7e003b9dc485f05d7db513892232f45f51de8eb
     </div>
   );
 };
