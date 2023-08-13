302a4d94c17e1ca26e61ffa2c1205aa01b76326c
KenanGuluzada <kananguluzadeh@gmail.com>
2023-08-13 16:13:50 +0400

    form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import PopularProdCart from "./components/PopularProdCart";
+import  Form  from "./components/Form";
 
 const App = () => {
   return (
     <div>
       <Header />
-<<<<<<< HEAD
-      <Footer/>
-      <PopularProdCart/>
-=======
       <Footer />
->>>>>>> d7e003b9dc485f05d7db513892232f45f51de8eb
+      <Form />
     </div>
   );
 };
