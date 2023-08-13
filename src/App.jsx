d7e003b9dc485f05d7db513892232f45f51de8eb
KenanGuluzada <kananguluzadeh@gmail.com>
2023-08-13 15:04:17 +0400

    button component design added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
-
 const App = () => {
   return (
     <div>
       <Header />
-      <Footer/>
+      <Footer />
     </div>
   );
 };
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+export const Button = (props) => {
+  const buttonStyle = {
+    backgroundColor: props.variant === "fill" ? "#1E4C2F" : "transparent",
+    border: props.variant === "outline" ? "1px solid #1E4C2F" : "none",
+    color: props.variant === "fill" ? "white" : "#1E4C2F",
+    borderRadius: props.size === "sm" ? "8px" : "8px",
+    width:
+      props.size === "xs" ? "120px" : props.size === "sm" ? "122px" : "53px",
+    height: props.size === "sm" ? "45px" : "53px",
+    fontSize: props.size === "sm" ? "18px" : "18px",
+    fontWeight: props.size === "sm" ? "600" : "600",
+  };
   return (
     <div>
-      
+      <button style={buttonStyle}>{props.text}</button>
     </div>
-  )
-}
-
-export default Button
+  );
+};
